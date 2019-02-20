508b4ed976fb0d2d094e6d89536b89a59549dfdf
Huang <41145779+Trebor-Huang@users.noreply.github.com>
2019-02-21 04:18:29 +0800

    Specify HoTTcore.__init__.py behavior
    Added the import behavior for .infrastructure and .unification part.
    Trying to think of a way to implement ITT.

---
+++ HoTTcore/ITT.py
@@ -0,0 +1,8 @@
+from HoTTcore.infrastructure import *
+from HoTTcore.unification import *
+
+"""Implements Intuitionistic Type Theory."""
+
+
+class ITTObj:
+    pass
---
+++ HoTTcore/__init__.py
@@ -0,0 +1,2 @@
+from .infrastructure import Expression, Variable, Constructor, Function
+from .unification import equation2str, unify
