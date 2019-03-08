9decd72ffbf76a0f19d238e6627b87275f5af0e7
Huang <41145779+Trebor-Huang@users.noreply.github.com>
2019-03-08 13:25:56 +0800

    Fourth Trial!!!!

--- HoTTcore/lambda_calculus.py
+++ HoTTcore/lambda_calculus.py
@@ -1,2 +1,25 @@
 """Implements lambda calculus."""
 from .basic import *
+
+
+class To(Constructor):
+    def __init__(self, intype: tuple, outtype):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __call__(self, *variables):
+        pass
+
+    def __hash__(self):
+        pass
+
+# TODO a new type for bounded variable
+
+
+class Lambda:
+    pass
